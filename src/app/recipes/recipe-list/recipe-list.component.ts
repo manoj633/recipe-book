dd1b068b80952d8d1941a295a244136d66f0a3ff
Manoj <manoj09633@gmail.com>
2022-07-31 16:35:08 +0530

    Data passed two level up to a parent component and then to a child component to a parent component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -11,9 +11,14 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe("A test recipe","This is simply a test","https://bit.ly/3zEzXX6")
   ];
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>(); 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
