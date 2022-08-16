63696fb31b98b0d4209f66fe090b4ef899c6b007
Manoj <manoj09633@gmail.com>
2022-08-16 17:19:38 +0530

    Added ingredients to recipes

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
@@ -6,8 +7,20 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   
   private recipes: Recipe[] = [
-    new Recipe("A test recipe","This is simply a test","https://bit.ly/3zEzXX6"),
-    new Recipe("A test recipe","This is simply a test","https://bit.ly/3zEzXX6")
+    new Recipe("A test recipe",
+    "This is simply a test",
+    "https://bit.ly/3zEzXX6",
+    [
+      new Ingredient('Meat', 1),
+      new Ingredient('Jalapeno',2)
+    ]),
+    new Recipe("Big fat burger",
+    "This is simply a test",
+    "https://bit.ly/3zEzXX6",
+    [
+      new Ingredient('Pepper', 1),
+      new Ingredient('Bread',2)
+    ])
   ];
 
   getRecipes() {
