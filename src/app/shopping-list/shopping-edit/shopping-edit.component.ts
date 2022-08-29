a1d5895fb3386b351fc1d8506ddd43d41209b8d5
Manoj <manoj09633@gmail.com>
2022-08-29 07:59:06 +0530

    Form reset after adding/updating ingredient

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -31,7 +31,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     });
   }
 
-  onAddItem(form: NgForm) {
+  onSubmit(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     if (this.editMode) {
@@ -39,6 +39,8 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     } else {
       this.slService.addIngredient(newIngredient);
     }
+    this.editMode = false;
+    this.slForm.reset();
   }
 
   ngOnDestroy(): void {
