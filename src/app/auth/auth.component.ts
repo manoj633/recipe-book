c0abc3276c9e12a2e5fbb44f6d8a6922c1c9a7f9
Manoj <manoj09633@gmail.com>
2022-10-06 11:24:56 +0530

    Switch between login and signup added

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -7,4 +7,10 @@ import { Component } from "@angular/core";
 })
 export class AuthComponent {
 
+    isLoginMode = true;
+
+    onSwitchMode() {
+        this.isLoginMode = !this.isLoginMode;
+    }
+
 }
