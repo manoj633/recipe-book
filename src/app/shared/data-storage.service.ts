7d4d952d1d26b552da26a47d7c81d783697db254
Manoj <manoj09633@gmail.com>
2022-10-07 14:07:40 +0530

    Added authToken to outgoing requests

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,12 +1,13 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { map, tap } from "rxjs/operators";
+import { exhaustMap, map, take, tap } from "rxjs/operators";
+import { AuthService } from "../auth/auth.service";
 import { Recipe } from "../recipes/recipe.model";
 import { RecipeService } from "../recipes/recipes.service";
 
 @Injectable()
 export class DataStorageService {
-    constructor(private http: HttpClient, private recipesService: RecipeService) { }
+    constructor(private http: HttpClient, private recipesService: RecipeService, private authService: AuthService) { }
 
     storeRecipes() {
         const recipes = this.recipesService.getRecipes();
@@ -14,7 +15,9 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        return this.http.get<Recipe[]>('firebase_url/recipes.json').pipe(map(recipes => {
+        return this.authService.user$.pipe(take(1), exhaustMap(user => {
+            return this.http.get<Recipe[]>('firebase_url/recipes.json', { params: new HttpParams().set('auth', user.token) })
+        }), map(recipes => {
             return recipes.map(recipe => {
                 return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] }
             })
