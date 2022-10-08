eecc6e84b5f70543740dbb5307ffa3a8ccc398c8
Manoj <manoj09633@gmail.com>
2022-10-08 12:47:34 +0530

    Removed unused import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { AuthGuard } from './auth/auth.guard';
