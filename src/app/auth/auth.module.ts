e031e503678cf4752be4b525083d21e6d0f8a43b
Manoj <manoj09633@gmail.com>
2022-11-01 15:16:59 +0530

    Authentication routes moved to feature module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
 import { SharedModule } from "../shared/shared.module";
 import { AuthComponent } from "./auth.component";
 
@@ -9,7 +10,8 @@ import { AuthComponent } from "./auth.component";
     ],
     imports: [
         FormsModule,
-        SharedModule
+        SharedModule,
+        RouterModule.forChild([{ path: 'auth', component: AuthComponent }]),
     ],
     exports: [
         AuthComponent
